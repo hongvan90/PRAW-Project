a5d9849503dc64614bc61aad37cc460de3022d81
Tiago Severino <tiago@severino.xyz>
2018-10-22 20:00:56 +0100

    Fix Typo

--- RandomSOTD.py
+++ RandomSOTD.py
@@ -1,7 +1,7 @@
 import praw, pprint
 user_agent = "Gilda" ##input("Please input your desired user agent to display to reddit: ")
 r = praw.Reddit(user_agent=user_agent)
-nsfwCheck = input("Allow NSFW subs to be displayed?(True or False): ")
+nsfwCheck = input("Allow NSFW subs to be displayed? (True or False): ")
 subreddit = r.get_random_subreddit(nsfw=nsfwCheck)
 pprint.pprint(subreddit)
 ##pls = pprint.pprint(subreddit)
